f69019e89ee9531f4d80057ddf7feba2d3f6066b
gentlechj <415250506@qq.com>
2022-12-03 23:51:18 +0800

    abstract poller as base class

--- src/poller.h
+++ src/poller.h
@@ -1,14 +1,7 @@
 #pragma once
-#include <sys/time.h>
-
-#include <map>
 #include <vector>
 
-#include "eventloop.h"
 #include "util.h"
-
-struct pollfd;
-
 namespace easynet {
 class Channel;
 
@@ -16,30 +9,17 @@ class Channel;
 class Poller : private noncopyable {
  public:
   typedef std::vector<Channel*> ChannelList;
-
-  Poller(EventLoop* loop);
-  ~Poller();
+  Poller() = default;
+  virtual ~Poller() = default;
 
   // Polls IO 事件
-  void poll(int timeoutMs, ChannelList* activeChannels);
+  virtual void poll(int timeoutMs, ChannelList* activeChannels) = 0;
 
   // 改变Channel感兴趣的IO事件
-  void updateChannel(Channel* channel);
-  
+  virtual void updateChannel(Channel* channel) = 0;
+
   // 移除channel
-  void removeChannel(Channel* channel);
-
-  void assertInLoopThread() { m_loop->assertInLoopThread(); };
-
- private:
-  // 找出活动的channel，放入activeChannels
-  void fillActiveChannels(int numEvents, ChannelList* activeChannels);
-
-  typedef std::vector<struct pollfd> PollfdList;
-  typedef std::map<int, Channel*> ChannelMap;
-
-  EventLoop* m_loop;
-  PollfdList m_pollfds;   // 缓存poll的fd
-  ChannelMap m_channels;  // fd到Channel* 的映射
+  virtual void removeChannel(Channel* channel) = 0;
 };
+
 }  // namespace easynet
